387ee07b7ba9c0da092e8dcadb53db554f60daa9
ismailcanuslu <isocan5561@gmail.com>
2022-09-23 11:37:43 +0300

    JavaCamp2022

---
+++ JavaCamp2022-Week1/helloWorld/src/helloWorld/helloWorld.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java2");
+
+	}
+
+}

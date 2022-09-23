cd80bca223776cc1162d8178843accebcbf440f7
ismailcanuslu <isocan5561@gmail.com>
2022-09-23 13:28:42 +0300

    JavaCamp2022

--- JavaCamp2022-Week1/switchDemo/src/switchDemo/Main.java
+++ JavaCamp2022-Week1/switchDemo/src/switchDemo/Main.java
@@ -3,7 +3,24 @@ package switchDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		char grade = 'A';
+		
+		switch(grade){
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+		case 'B':
+			System.out.println("İyi : Geçtiniz");
+			break;
+		case 'C':
+			System.out.println("Orta : Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Malesef : Kaldınız");
+			break;
+			default:
+				System.out.println("Geçersiz not girdiniz");
+		}
 
 	}
 

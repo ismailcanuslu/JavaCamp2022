fbb2b4d831557358e531bd54fc81e8ecd50a8bb1
ismailcanuslu <isocan5561@gmail.com>
2022-09-23 11:53:01 +0300

    JavaCamp2022

---
+++ JavaCamp2022-Week1/dataTypes/src/dataTypes/dataTypes.java
@@ -0,0 +1,15 @@
+package dataTypes;
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+		double sayi = 12.5;
+		sayi = -129;
+		
+		char karakter = 'A';
+		
+		boolean dogruMu = false;
+
+	}
+
+}

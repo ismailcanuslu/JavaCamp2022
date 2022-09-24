2cf01fe4e63a2bbbcdb8c4f5adc645aec5b4b408
ismailcanuslu <isocan5561@gmail.com>
2022-09-24 11:36:52 +0300

    JavaCamp2022

---
+++ JavaCamp2022-Week2/classes/src/classes/Main.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager;
+		CustomerManager customerManager2 = new CustomerManager();
+		customerManager = customerManager2;
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+	}
+
+}

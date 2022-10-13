5d5dee2c98f9180be923f7c301bad3ac98704f70
ismailcanuslu <isocan5561@gmail.com>
2022-10-13 14:11:38 +0300

    JavaCamp2022
    JavaCamp2022

---
+++ JavaCamp2022-Week3/abstractClasses/src/abstractClasses/ManGameCalculator.java
@@ -0,0 +1,8 @@
+package abstractClasses;
+
+public class ManGameCalculator extends GameCalculator {
+	
+	public void Hesapla() {
+		System.out.println("Puanınız : 90");
+	}
+}
---
+++ JavaCamp2022-Week3/abstractClasses/src/abstractClasses/OlderGameCalculator.java
@@ -0,0 +1,12 @@
+package abstractClasses;
+
+public class OlderGameCalculator extends GameCalculator{
+
+	@Override
+	public void Hesapla() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
